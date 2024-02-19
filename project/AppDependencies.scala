6430c8212dd4b712da307b62ad05e218ae4527a6
CarolineS-QA <62263396+CarolineS-QA@users.noreply.github.com>
2024-02-19 22:23:00 +0000

    APB-7777 [CS] tudor crown update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,13 +1,13 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
   private val mongoVersion = "0.74.0"
-  private val bootstrapVer = "7.22.0"
+  private val bootstrapVer = "7.23.0"
 
-  lazy val compile = Seq(
+  lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-28"    % bootstrapVer,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc"            % "7.7.0-play-28",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-28"    % "8.5.0",
     "uk.gov.hmrc"           %% "play-partials"                 % "8.4.0-play-28",
     "uk.gov.hmrc"           %% "agent-kenshoo-monitoring"      % "5.5.0",
     "uk.gov.hmrc"           %% "agent-mtd-identifiers"         % "1.15.0",
@@ -17,11 +17,11 @@ object AppDependencies {
     "com.github.tototoshi"  %% "scala-csv"                     % "1.3.10"
   )
 
-  lazy val test = Seq(
+  lazy val test: Seq[ModuleID] = Seq(
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"      % "test, it",
     "org.scalatestplus"      %% "mockito-3-12"            % "3.2.10.0"   % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8"            % "2.26.1"     % "test, it",
-    "org.jsoup"              % "jsoup"                    % "1.15.4"     % "test, it",
+    "com.github.tomakehurst" %  "wiremock-jre8"           % "2.26.1"     % "test, it",
+    "org.jsoup"              %  "jsoup"                   % "1.15.4"     % "test, it",
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % mongoVersion % "test, it",
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapVer % "test, it",
     "com.vladsch.flexmark"   %  "flexmark-all"            % "0.36.8"    % "test, it"
