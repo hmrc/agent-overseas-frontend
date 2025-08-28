c6001f770b141c6c621d27dae71768e6dcea2fe8
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2025-08-28 14:26:39 +0100

    APB-9857 dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,23 +2,23 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoVer: String = "2.6.0"
-  private val bootstrapVer: String = "9.16.0"
+  private val mongoVer: String = "2.7.0"
+  private val bootstrapVer: String = "9.19.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"    % bootstrapVer,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"    % "12.7.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"    % "12.8.0",
     "uk.gov.hmrc"           %% "agent-mtd-identifiers"         % "2.2.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"            % mongoVer,
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "org.typelevel"         %% "cats-core"                     % "2.13.0",
-    "uk.gov.hmrc"           %% "crypto-json-play-30"           % "8.2.0"
+    "uk.gov.hmrc"           %% "crypto-json-play-30"           % "8.3.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVer % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoVer % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.1"      % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2"      % Test,
   )
 
 }
