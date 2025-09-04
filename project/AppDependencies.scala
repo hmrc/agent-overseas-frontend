d78f52ba0223e18422fc28a52120ecd0ae3da6a1
Wojciech Gradzki <40370552+gitwojciech@users.noreply.github.com>
2025-09-04 09:10:41 +0100

    [WG][APB-9827] Update dependencies (#96)
    * [WG][APB-9827] dependencies update
    
    * [WG][APB-9827] dependencies update, repoint MDC to new version
    
    ---------
    
    Co-authored-by: wojciech.gradzki <gitwojciech@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object AppDependencies {
 
   private val mongoVer: String = "2.7.0"
-  private val bootstrapVer: String = "9.19.0"
+  private val bootstrapVer: String = "10.1.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"    % bootstrapVer,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"    % "12.8.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"    % "12.10.0",
     "uk.gov.hmrc"           %% "agent-mtd-identifiers"         % "2.2.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"            % mongoVer,
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.3.0",
