f02eba45c50f6c6570870b9322a5fc973611a2ac
raw1697 <romel.w@hotmail.co.uk>
2026-06-25 09:30:34 +0100

    APB-11674 - Address dependency vulnerabilities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,20 +4,21 @@ object AppDependencies {
 
   private val mongoVer: String = "2.12.0"
   private val bootstrapVer: String = "10.7.0"
+  private val playVer: String = "play-30"
 
   lazy val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"            % bootstrapVer,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"            % "12.32.0",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                    % mongoVer,
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30" % "3.5.0",
+    "uk.gov.hmrc"           %% s"bootstrap-frontend-$playVer"            % bootstrapVer,
+    "uk.gov.hmrc"           %% s"play-frontend-hmrc-$playVer"            % "12.32.0",
+    "uk.gov.hmrc.mongo"     %% s"hmrc-mongo-$playVer"                    % mongoVer,
+    "uk.gov.hmrc"           %% s"play-conditional-form-mapping-$playVer" % "3.5.0",
     "org.typelevel"         %% "cats-core"                             % "2.13.0",
-    "uk.gov.hmrc"           %% "crypto-json-play-30"                   % "8.4.0",
-    "uk.gov.hmrc"           %% "domain-play-30"                        % "11.0.0"
+    "uk.gov.hmrc"           %% s"crypto-json-$playVer"                   % "8.4.0",
+    "uk.gov.hmrc"           %% s"domain-$playVer"                        % "11.0.0"
   )
 
   lazy val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVer % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoVer     % Test,
+    "uk.gov.hmrc"            %% s"bootstrap-test-$playVer"  % bootstrapVer % Test,
+    "uk.gov.hmrc.mongo"      %% s"hmrc-mongo-test-$playVer" % mongoVer     % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.2"      % Test,
   )
 
